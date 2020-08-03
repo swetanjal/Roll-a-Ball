9165fd9feb9504d07f740eb6f393a2a842b68373
swetanjal <swetanjal.dutta@research.iiit.ac.in>
2020-08-03 22:51:06 +0530

    Commit game.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Roll-a-Ball
+
+## How to run the game:
+-  Open the terminal in the current directory and type the command: ./Roll-a-Ball/Builds/Roll-a-Ball.x86_64
+
+## Controls:
+- Use the arrow keys to move the ball and collect the diamonds.
---
+++ Roll-a-Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+    private int count = 0;
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    public GameObject winTextObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMove(InputValue movementValue){
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 12){
+            winTextObject.SetActive(true);
+        }
+    }
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("PickUp")){
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+        
+    }
+}
